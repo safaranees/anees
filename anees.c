0021225f348fb25e25419168cd7d7c424c672367
safaranees <aneesparvin08@gmail.com>
2017-06-28 14:53:51 +0530

    Create anees.c

---
+++ anees.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n enter the number");
+scanf("%d\n",&a);
+if(a>0)
+{
+printf("\n the given no is positive");
+}
+else if(a==0)
+{
+printf("|n the given no is zero");
+}
+else
+{
+printf("\n the given no is negative");
+]
+getch();
+]
