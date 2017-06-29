a933a4a761e60dd05bebf16c05c5904695ea598c
safaranees <aneesparvin08@gmail.com>
2017-06-29 14:48:06 +0530

    Create ani.c

---
+++ ani.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n enter the number");
+scanf("\n %d ",&a);
+if(a%2==0)
+{
+printf("|n the given no is even");
+}
+else
+{
+printf("|n the given no is odd");
+}
+getch();
+}
