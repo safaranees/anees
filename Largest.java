bcb2fed916b381e5e2331e5a1ee0bd498656c813
safaranees <aneesparvin08@gmail.com>
2018-08-20 13:51:52 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Largest
+{
+public static void main(String args[])
+{
+int a=4,b=2,c=3;
+if(a>b && a>c)
+{
+ System.out.println("a is greater");
+}
+else if(b>c)
+{
+System.out.println("b is greater");
+}
+else
+{
+ System.out.println("c is greater");
+}
+}
+}
