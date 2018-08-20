8d27682a8dea3c37b7408814f6a9641b4fe5036b
safaranees <aneesparvin08@gmail.com>
2018-08-20 13:58:15 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+mport java.io.*;
+import java.util.*;
+class Hello
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=0;i<n;i++)
+{
+   System.out.println("Hello");
+}
+}
+}
