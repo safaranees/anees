0eb06270f9d3241f87f079355d736fcc5ef7fb5c
safaranees <aneesparvin08@gmail.com>
2018-08-25 17:58:23 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -4,7 +4,6 @@ class Array
 {
 public static void main(String args[])
 {
-int n[]=new int[10];
 int n[]={2,3,5,7,9,4,6};
 int k=2;
 int sum=0;
@@ -13,4 +12,5 @@ for(int i=0;i<k;i++)
 sum=sum+n[i];
 }
 System.out.println(sum");
+}
 }
