ae7efa9ecea8fb2ac5e699a45240a8f83d3bbca2
safaranees <aneesparvin08@gmail.com>
2018-08-25 17:48:48 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Array
+{
+public static void main(String args[])
+{
+int n[]=new int[10];
+int n[]={2,3,5,7,9,4,6};
+int k=2;
+int sum=0;
+for(int i=0;i<k;i++)
+{
+sum=sum+n[i];
+}
+System.out.println(sum");
+}
