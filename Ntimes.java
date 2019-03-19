d6a93e1f98bc273dcc8fff7b6289c0d7a0a4c924
safaranees <aneesparvin08@gmail.com>
2019-03-19 18:26:17 +0530

    Update and rename Hello.java to Ntimes.java

---
+++ Ntimes.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Ntimes
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		for(int i=0;i<a;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
