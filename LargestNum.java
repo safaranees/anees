2c44a7b1de974700decf67e2728cdac3205ecbe9
safaranees <aneesparvin08@gmail.com>
2019-03-19 18:18:20 +0530

    Update and rename Largest.java to LargestNum.java

---
+++ LargestNum.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class LargestNum
+{
+	public static void main(String args[])
+	{
+		System.out.println("enter the numbers");
+		Scanner num=new Scanner(System.in);
+		int num1=num.nextInt();
+		int num2=num.nextInt();
+		int num3=num.nextInt();
+		if(num1>num2 && num1>num3)
+		{
+			System.out.println("num1 is greater");
+		}
+		else if(num2>num3)
+		{
+			System.out.println("num2 is greater");
+		}
+		else
+		{
+			System.out.println("num3 is greater");
+		}
+	}
+}
